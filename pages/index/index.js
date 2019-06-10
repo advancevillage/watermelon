d10073421d7c8efaa5ddb00af25830fdade83ee0
advancevillage <cugriver@gmail.com>
2019-06-10 18:34:56 +0800

    #richard
    微信小程序动工

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,22 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    navigate_items_array: {}
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  onLoad: function() {
+    var self = this;
+    wx.request({
+      url: 'http://localhost:13147/',
+      method: 'GET',
+      dataType: 'json',
+      success: function (response) {
+        let api_data  = response.data;
+        let api_error = response.data['error'];
+        let api_data_navigate = api_data['data']['navigate'] || {}
+        self.setData({ navigate_items_array: api_data_navigate});
+      },
+      fail: function (response) { console.log(response)},
+      complete: function (response) { console.log(response)}
+    });
   }
 })
